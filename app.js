c89bced5f39d7a85906545f2bebace4b3166f01d
dqkhang96 <dqkhang96@gmail.com>
2019-10-30 11:40:40 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app=express()
+
+app.get('/api/user',(req,res)=>{
+    res.status(200).send({
+        success: 'true',
+        message: 'todos retrieved successfully'
+      })
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
